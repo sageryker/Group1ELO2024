01d112caa10c7a08b596137ec5b72cfccc2497bf
sageryker <149901186+sageryker@users.noreply.github.com>
2024-12-16 19:01:09 -0500

    Lesson plan goes brr x 2

--- Drivetrain.java
+++ Drivetrain.java
@@ -1,1 +1,17 @@
+// Motor initialization
 
+// Differential Drive
+
+//Voltage Limits - in code
+
+//Slew Rate Limiter (The primitive motion profile)
+
+//Encoders software vs. harware
+
+//Tank Drive
+
+//Arcade Drive
+
+//How do YOU think we could switch between them?
+
+//Pause in programming for mini lesson on swerve
